70d076d8428bb2e551415b0bba777193cb6ca582
BhanupratapsinghVE <bhanuve2001@gmail.com>
2023-08-08 17:15:58 +0530

    Registrtion page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './App.css';
 import Navbar from './Components/Header/Navbar';
 import Quiz from './Components/Quiz/Quiz';
-
+import Signup from './Components/Registration/Signup.jsx';
+import Sidebar from './Components/Sidebar/Sidebar';
 function App() {
   return (
     <div className="App">
-  
-    <Routes>
-      <Route path="/" element={<Navbar/>}/>
-      <Route path='/quiz' element={<Quiz/>}/>
-    </Routes>
-   
+
+      <Routes>
+        <Route path="/" element={<Navbar />} />
+        <Route path='/quiz' element={<Quiz />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/sidebar' element={<Sidebar />} />
+      </Routes>
+
     </div>
   );
 }
---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <>
+      <div>
+        <div className="navbar">
+          <ul className="nav-links">
+            <li>
+              <a href="#">Explore</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
