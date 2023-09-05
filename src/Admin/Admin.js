0b4ed050eb081844222c485e2e4de229c1893cbd
AnmolVE <anve045@gmail.com>
2023-09-05 12:31:15 +0530

    Modifications in admin page

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,43 +1,69 @@
-import React, {useContext} from 'react'
-import './Admin.css'
-import AuthContext from '../Components/Context/AuthContext'
+import React, { useContext } from "react";
+import "./Admin.css";
+import AuthContext from "../Components/Context/AuthContext";
 const Admin = () => {
-  const {logoutUser} = useContext(AuthContext)
+  const { logoutUser } = useContext(AuthContext);
   return (
     <div>
-    
-  <body>
-    <main>
-      <header>
-        <h2  className='h11'>Admin Dashboard</h2>
-      </header>
-      <section class="row">
-        <article class="col-md-3 article-left">
-<i class="fa fa-heart-o fa-5x" aria-hidden="true"></i>
-        <h2 class="text-center title">Admin Dashboard</h2>
-        <ul class="ul">
-          <li><i class="fa fa-home" aria-hidden="true"></i><a href="#">Home</a></li>
-          <li><i class="fa fa-comments" aria-hidden="true"></i><a href="#">Messages</a></li>
-          <li><i class="fa fa-user-o" aria-hidden="true"></i><a href="#">Users</a></li>
-          <li><i class="fa fa-rocket" aria-hidden="true"></i><a href="#">To-Do</a></li>
-          <li><i class="fa fa-pie-chart" aria-hidden="true"></i><a href="#">Statistics</a></li>
-          <li><i class="fa fa-cogs" aria-hidden="true"></i><a href="#">Settings</a></li>
-          <li><i class="fa fa-line-chart" aria-hidden="true"></i><a href="#">Analytics</a></li>
-          <li><i class="fa fa-facebook" aria-hidden="true"></i><a href="#">Social</a></li>
-          <li><i class="fa fa-rss" aria-hidden="true"></i><a href="#">Feedback</a></li>
-        </ul>
-      </article>
-      <article class="col-md-9 article-right">
-        {/* <h1  className='h112'>Admin Panel</h1> */}
-        
-       </article>
-       <button class="login-btn" onClick={logoutUser}>Logout</button>
-      </section>
-     </main>
-  </body>
-
+      <body>
+        <main>
+          {/* <header>
+            <h2 className="h11">Admin Dashboard</h2>
+          </header> */}
+          <section class="row">
+            <article class="col-md-3 article-left">
+              <i class="fa fa-heart-o fa-5x" aria-hidden="true"></i>
+              {/* <h2 class="text-center title">Admin Dashboard</h2> */}
+              <ul class="ul">
+                <li>
+                  <i class="fa fa-home" aria-hidden="true"></i>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <i class="fa fa-comments" aria-hidden="true"></i>
+                  <a href="#">Messages</a>
+                </li>
+                <li>
+                  <i class="fa fa-user-o" aria-hidden="true"></i>
+                  <a href="#">Users</a>
+                </li>
+                <li>
+                  <i class="fa fa-rocket" aria-hidden="true"></i>
+                  <a href="#">To-Do</a>
+                </li>
+                <li>
+                  <i class="fa fa-pie-chart" aria-hidden="true"></i>
+                  <a href="#">Statistics</a>
+                </li>
+                <li>
+                  <i class="fa fa-cogs" aria-hidden="true"></i>
+                  <a href="#">Settings</a>
+                </li>
+                <li>
+                  <i class="fa fa-line-chart" aria-hidden="true"></i>
+                  <a href="#">Analytics</a>
+                </li>
+                <li>
+                  <i class="fa fa-facebook" aria-hidden="true"></i>
+                  <a href="#">Social</a>
+                </li>
+                <li>
+                  <i class="fa fa-rss" aria-hidden="true"></i>
+                  <a href="#">Feedback</a>
+                </li>
+              </ul>
+            </article>
+            <article class="col-md-9 article-right">
+              {/* <h1  className='h112'>Admin Panel</h1> */}
+            </article>
+            <button class="login-btn" onClick={logoutUser}>
+              Logout
+            </button>
+          </section>
+        </main>
+      </body>
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
--- src/Components/Editors/EditorDetails/ProfileDetails.js
+++ src/Components/Editors/EditorDetails/ProfileDetails.js
@@ -1,13 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { useContext } from 'react';
-import AuthContext from '../../Context/AuthContext';
-import { Card, CardContent, Typography, IconButton, CardActions } from '@mui/material';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
-import './ProfileDetails.css';
-import Navbar from '../../Header/Navbar';
-import Footer from '../../Footer/footer';
-import Admin from '../../../Admin/Admin';
+import React, { useState, useEffect } from "react";
+import { useContext } from "react";
+import AuthContext from "../../Context/AuthContext";
+import {
+  Card,
+  CardContent,
+  Typography,
+  IconButton,
+  CardActions,
+} from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import "./ProfileDetails.css";
+import Navbar from "../../Header/Navbar";
+import Footer from "../../Footer/footer";
+import Admin from "../../../Admin/Admin";
 
 const ProfileDetails = () => {
   const [data, setData] = useState([]);
@@ -16,68 +22,67 @@ const ProfileDetails = () => {
   useEffect(() => {
     fetch("http://127.0.0.1:8000/api/user-data/", {
       headers: {
-        Authorization: `Bearer ${authTokens.tokens.access}`
-      }
+        Authorization: `Bearer ${authTokens.tokens.access}`,
+      },
     })
       .then((result) => result.json())
       .then((resp) => {
         setData(resp);
         console.log(resp);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error fetching data:", error);
       });
   }, []);
 
   // Filter data based on user role which we are getting from Authcontext
 
-  const filteredData = data.filter(item => {
-    if (authTokens.tokens.role === 'Editor') {
-      return item.role === 'Editor';
-    } else if (authTokens.tokens.role === 'User') {
-      return item.role === 'User';
+  const filteredData = data.filter((item) => {
+    if (authTokens.tokens.role === "Editor") {
+      return item.role === "Editor";
+    } else if (authTokens.tokens.role === "User") {
+      return item.role === "User";
     }
     return true;
   });
 
   return (
     <>
-    <div className='newfloat'>
-    <Admin/>
-    <div>
-    <h2 className='nameNew h11'>Welcome to the Admin-Panel</h2>
-    <div className="dashboard">
-    
-      {filteredData.map((item, i) => (
-        <Card key={i} className="dashboard-card">
-          <h3>{item.role}</h3>
-          <CardContent>
-            <Typography variant="h6" className='namehuh'>
-              {item.username}
-            </Typography>
-            <Typography variant="body2" className='about'>
-              {item.about}
-            </Typography>
-            <Typography variant="body2" className='email'>
-              Email: {item.email}
-            </Typography>
-            <Typography variant="body2" className='role'>
-              Role: {item.role}
-            </Typography>
-          </CardContent>
-          <CardActions className="dashboard-card-actions">
-            <IconButton aria-label="edit" color="primary">
-              <EditIcon />
-            </IconButton>
-            <IconButton aria-label="delete" color="secondary">
-              <DeleteIcon />
-            </IconButton>
-          </CardActions>
-        </Card>
-      ))}
-    </div>
-    </div>
-    </div>
+      <div className="newfloat">
+        <Admin />
+        <div>
+          <h2 className="nameNew h11">Welcome to the Admin-Panel</h2>
+          <div className="dashboard">
+            {filteredData.map((item, i) => (
+              <Card key={i} className="dashboard-card">
+                <h3>{item.role}</h3>
+                <CardContent>
+                  <Typography variant="h6" className="namehuh">
+                    {item.username}
+                  </Typography>
+                  <Typography variant="body2" className="about">
+                    {item.about}
+                  </Typography>
+                  <Typography variant="body2" className="email">
+                    Email: {item.email}
+                  </Typography>
+                  <Typography variant="body2" className="role">
+                    Role: {item.role}
+                  </Typography>
+                </CardContent>
+                <CardActions className="dashboard-card-actions">
+                  <IconButton aria-label="edit" color="primary">
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton aria-label="delete" color="secondary">
+                    <DeleteIcon />
+                  </IconButton>
+                </CardActions>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
